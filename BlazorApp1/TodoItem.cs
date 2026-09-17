b814fb2fa322bbf605aee94385d7d7d2ec66de93
Developer <dev@example.com>
2026-09-17 08:28:52 +0700

    them Todo list

---
+++ BlazorApp1/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp_Demo
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
---
+++ TodoItem/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoItem
+{
+    public class Class1
+    {
+
+    }
+}
